0c6b643e92e548039e64bfb923f0188f20a19225
Akhilesh Kumawat <akhileshkuw9@gmail.com>
2025-03-19 11:05:35 +0530

    maked

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    phone: { type: String, required: true },
-    portfolio: { type: String, required: true }
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 12,
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    portfolio: {
+        type: String,
+        required: true,
+    }
 });
-
 
 const User = mongoose.model("User", userSchema);
 
