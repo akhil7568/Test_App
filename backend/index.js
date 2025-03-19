18c89925185a46e4d6cc0b7ed9ba6879a3eae33e
Akhilesh Kumawat <akhileshkuw9@gmail.com>
2025-03-19 12:23:31 +0530

    Hello

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const User = require('./models/userModel')
 const app = express();
 app.use(express.json());
 dotenv.config();
